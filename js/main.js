e9e87e9abb2279918cedafc0ceac3370b3cbf5bf
Kaylee Menard <mena0150@algonquinlive.com>
2015-03-04 16:27:15 -0500

    Created collapse/expand animation

--- js/main.js
+++ js/main.js
@@ -4,10 +4,17 @@ var $box = $('.box');
 var $btnMove = $('.btn-move');
 var $diamond = $('.diamond');
 
+var $btnExpand = $('.btn-collapse-expand');
+var $panel = $('.panel');
+
 $btnShowHide.on('click', function() {
 	$box.toggleClass('js-show-hide');
 })
 
 $btnMove.on('click', function() {
 	$diamond.toggleClass('js-move');
+})
+
+$btnExpand.on('click', function() {
+	$panel.toggleClass('js-expand');
 })
