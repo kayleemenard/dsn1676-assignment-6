6aaca48313243ec5b25a441e32d2495cacaaad50
Kaylee Menard <mena0150@algonquinlive.com>
2015-03-04 16:18:53 -0500

    Created move in/out

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 var $btnShowHide = $('.btn-show-hide');
 var $box = $('.box');
 
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+
 $btnShowHide.on('click', function() {
 	$box.toggleClass('js-show-hide');
+})
+
+$btnMove.on('click', function() {
+	$diamond.toggleClass('js-move');
 })
